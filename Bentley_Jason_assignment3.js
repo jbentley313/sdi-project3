e33c7803b478eb1b1a3766ecb745a134e860a1af
Jason Bentley <jbxfade@fullsail.edu>
2012-05-14 22:59:59 -0700

    Updated json.js, ran some tests, backup

--- Bentley_Jason_assignment3.js
+++ Bentley_Jason_assignment3.js
@@ -1,1 +1,16 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+// Assignment: Project 3
+// Author: Jason Bentley
+// Created for: SDI Online 1205
+// First assignment: Project 3: Bar hopping with coworkers
+
+var displayJson = function function (json) {
+	for (var i = 0; i < json.coworkers.length; i++){
+		var coworker = json.coworkers[i];
+		console.log(coworker.name + " is " + coworker.age + " years old, and lives in " + coworker.city + ". His favorite beers are:" + coworker.favBeers + ".");
+	};
+};
+
+displayJson(json);
+
--- json.js
+++ json.js
@@ -7,22 +7,42 @@ var json = {
 		{
 			"name": "Jason",
 			"age": 33,
-			"city": "West Hollywood"
+			"city": "West Hollywood",
+			"favBeers": [
+				" Fat Tire", 
+				" Stella", 
+				" Pabst"
+			]
 		},
 		{
 			"name": "Kevin",
 			"age": 32,
-			"city": "Los Angeles"
+			"city": "Los Angeles",
+			"favBeers": [
+				" Blue Moon", 
+				" MGD", 
+				" Honey Brown"
+			]
 		},
 		{
 			"name": "Joel",
 			"age": 39,
-			"city": "Los Angeles"
+			"city": "Los Angeles",
+			"favBeers": [
+				" Sunshine Wheat", 
+				" Coors", 
+				" Amstel"
+			]
 		},
 		{
 			"name": "Ben",
 			"age": 36,
-			"city": "San Diego"
+			"city": "San Diego",
+			"favBeers": [
+				" Miller Light", 
+				" Budweiser", 
+				" Corona"
+			]
 		}
 	]
 };
