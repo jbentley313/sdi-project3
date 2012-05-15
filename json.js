ba350f0dcc0a0a1dda54a45992cd45dd4c976d48
Jason Bentley <jbxfade@fullsail.edu>
2012-05-14 21:10:33 -0700

    Created json.js file

---
+++ json.js
@@ -0,0 +1,28 @@
+// JS Data for Project 3
+// Author: Jason Bentley
+// Data File
+
+var json = {
+	"coworkers": [
+		{
+			"name": "Jason",
+			"age": 33,
+			"city": "West Hollywood"
+		},
+		{
+			"name": "Kevin",
+			"age": 32,
+			"city": "Los Angeles"
+		},
+		{
+			"name": "Joel",
+			"age": 39,
+			"city": "Los Angeles"
+		},
+		{
+			"name": "Ben",
+			"age": 36,
+			"city": "San Diego"
+		}
+	]
+};
